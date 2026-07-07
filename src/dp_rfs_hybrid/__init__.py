250e9564c3d7fe58164c9311fd9401f4200e74d6
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-07 04:42:02 +0200

    Export DP clutter model

--- src/dp_rfs_hybrid/__init__.py
+++ src/dp_rfs_hybrid/__init__.py
@@ -1,13 +1,17 @@
 """DP/RFS hybrid tracking primitives."""
 
 from .dp_birth import BirthAtom, BirthDecision, DirichletProcessBirthModel
+from .dp_clutter import ClutterAtom, ClutterUpdate, DirichletProcessClutterModel
 from .gaussian import GaussianState, gaussian_pdf
 from .lmb_tracker import LabeledMultiBernoulliTracker, StepSummary, Track
 
 __all__ = [
     "BirthAtom",
     "BirthDecision",
+    "ClutterAtom",
+    "ClutterUpdate",
     "DirichletProcessBirthModel",
+    "DirichletProcessClutterModel",
     "GaussianState",
     "LabeledMultiBernoulliTracker",
     "StepSummary",
