2097069bdfdfe679f375c17aa690ac91965eed3e
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-09 00:55:22 +0200

    Export birth learning diagnostic

--- src/dp_rfs_hybrid/__init__.py
+++ src/dp_rfs_hybrid/__init__.py
@@ -1,6 +1,11 @@
 """DP/RFS hybrid tracking primitives."""
 
-from .dp_birth import BirthAtom, BirthDecision, DirichletProcessBirthModel
+from .dp_birth import (
+    BirthAtom,
+    BirthDecision,
+    BirthLearningUpdate,
+    DirichletProcessBirthModel,
+)
 from .dp_clutter import (
     ClutterAtom,
     ClutterUpdate,
@@ -20,6 +25,7 @@ from .lmb_tracker import LabeledMultiBernoulliTracker, StepSummary, Track
 __all__ = [
     "BirthAtom",
     "BirthDecision",
+    "BirthLearningUpdate",
     "ClutterAtom",
     "ClutterUpdate",
     "DirichletProcessBirthModel",
